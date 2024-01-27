693e11fd1126a44019875b8f0d410d2eabfecc9d
Tajbir islam <studenttajbirislam@gmail.com>
2024-01-27 12:02:46 +0600

    node js create server

--- server2.js
+++ server2.js
@@ -3,7 +3,8 @@ const port = 5000;
 const hostName = "127.0.0.1";
 
 const myServer = http.createServer((req,res) => {
-    res.writeHead(202, {'Content-Type': 'text/plain'});
+    res.writeHead(202, {'Content-Type': 'text/html'});
+    // res.writeHead(202, {'Content-Type': 'text/plain'});
     res.write('<h1>Hello world</h1>');
     res.end();
 })
