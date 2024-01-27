d3cea850a6f78db0370e7680a516e76b2a7e8fb3
Tajbir islam <studenttajbirislam@gmail.com>
2024-01-27 09:55:27 +0600

    node js import and export

---
+++ index.js
@@ -0,0 +1,6 @@
+const student = require("./student")
+
+console.log(student.getAge());
+console.log(student.getName())
+console.log(student.gpa())
+console.log(student.name)
---
+++ student.js
@@ -0,0 +1,20 @@
+const getName = () => {
+    return "Tajbir islam"
+}
+
+const getAge = () => {
+    return "20"
+}
+
+const gpa = () => {
+    return "4.25"
+}
+
+const name = "my name is tajbir islam"
+
+module.exports = {
+    getName,
+    getAge,
+    gpa,
+    name
+}
