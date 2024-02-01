a5959d7dda6cd531ad58467650e5fe8feaad681e
Tajbir islam <studenttajbirislam@gmail.com>
2024-02-01 20:31:17 +0600

    form data send to server

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get("/", (req,res)=>{
-    res.send('i am home route');
-    res.end()
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname+'/index.html')
+})
+
+app.post("/", (req,res)=>{
+    const formData = req.body
+    console.log(formData)
+    res.send(formData)
 })
 
 module.exports = app;
